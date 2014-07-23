718c90c67a601d50b2cc59391ddb5b556f353217
Carlos Contreras <carloscontrerasc@gmail.com>
2014-07-23 12:25:30 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion using cached memory 
+## Carlos Contreras
+## carlos.contreras@ualberta.ca
 
-## Write a short comment describing this function
+## Creates of special storage for the Mmtrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix  <- function(A = matrix()) {
+    Ainv <- NULL
+    setMatrix <- function(B) {
+        A <<- B
+        Ainv <<- NULL
+    }
+    getMatrix <- function() A
+    setInverse <- function(Ainvt) Ainv <<- Ainvt
+    getInverse <- function() Ainv
+    list(setMatrix = setMatrix, 
+         getMatrix = getMatrix, 
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of a matrix in sepcial storage, 
+## or retrieves the inverse if it has been already computd
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(A, ...) {
+    Ainv <- A$getInverse()
+    if(!is.null(Ainv)) {
+        message("retrieving inverse from cached data")
+        return(Ainv)
+    }
+    Mat <- A$getMatrix()
+    Ainv <- solve(Mat, ...)
+    A$setInverse(Ainv)
+    Ainv
 }
